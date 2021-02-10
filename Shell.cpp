f70d3fd1d9bcbabc91d1de56543ae0224cea62c9
Patrick Dohm <pd74@evansville.edu>
2021-02-10 13:39:16 -0600

    added child process

--- Shell.cpp
+++ Shell.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #define MAX_LINE 80 //The maximum length command
 using namespace std;
@@ -16,8 +17,17 @@ int main(void){
 
             //after reading user input the steps are :
             //(1) fork a child process using fork()
+            pid_t child_pid;
+            child_pid= fork();
             //(2) The child process will invoke execvp()
+            if (child_pid == 0){ //CHILD PROCESS
+                char* command= args[0];
+                int statuscode=execvp(command, args);
+                if(statuscode==-1){ //checking that execvp worked properly
+                    printf("process did not terminate correctly");
+                }
+            }
             //(3) If command included & parent will invoke wait
         }
-
+    return 0;
 }
