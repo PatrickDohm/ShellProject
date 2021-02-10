5d7e56732db9c5420bd22b955c29a8715e627df6
Patrick Dohm <pd74@evansville.edu>
2021-02-10 13:21:29 -0600

    added code from te book

--- Shell.cpp
+++ Shell.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <stdio.h>
 #include <unistd.h>
 
+#define MAX_LINE 80 //The maximum length command
 using namespace std;
 
-int main(int argc, char* argv[]){
+int main(void){
+
+    char* args[MAX_LINE/2 + 1]; //command line arguments
+    int should_run = 1; //flag to determine when to exit program
+
+        while(should_run){
+            printf("osh>");
+            fflush(stdout);
+
+            //after reading user input the steps are :
+            //(1) fork a child process using fork()
+            //(2) The child process will invoke execvp()
+            //(3) If command included & parent will invoke wait
+        }
 
 }
