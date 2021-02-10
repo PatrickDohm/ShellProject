4f29ac70b55c58add814c9a6501ce46393d0e2f0
Patrick Dohm <pd74@evansville.edu>
2021-02-10 14:05:30 -0600

    wrote a parsing function

--- Shell.cpp
+++ Shell.cpp
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string>
+#include <ctype.h>
+#include <signal.h>
 
 #define MAX_LINE 80 //The maximum length command
 using namespace std;
+
+void parse(char* line, char **args);
 
 int main(void){
 
@@ -16,6 +21,9 @@ int main(void){
             fflush(stdout);
 
             //after reading user input the steps are :
+            char line[MAX_LINE];
+            cin>>line;
+            parse(line, args);
             //(1) fork a child process using fork()
             pid_t child_pid;
             child_pid= fork();
@@ -24,10 +32,29 @@ int main(void){
                 char* command= args[0];
                 int statuscode=execvp(command, args);
                 if(statuscode==-1){ //checking that execvp worked properly
-                    printf("process did not terminate correctly");
+                    printf("process did not terminate correctly\n");
                 }
             }
             //(3) If command included & parent will invoke wait
+            if(child_pid>0){  //parent process
+
+            }
         }
     return 0;
+}
+
+
+//converts the line into args
+void parse(char* line, char **args){
+    while(*line != '\0'){
+        while(*line == ' ' || *line== '\t'){
+            *line++='\0';
+        }
+        *args++=line;
+        while(*line != '\0' && *line != ' ' 
+                && *line !='\t' && *line!= '\n'){
+                    line++;
+                }
+    }
+    *args= NULL;
 }
